7da8688f7ae47671776a2a7bbd0b0a785c76f610
Varunkashyap2303 <r.varunkashyap99@gmail.com>
2020-01-18 11:04:00 +0530

    Modified flutter_export_environment file and added config.dart file

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=C:\src\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\Admin\FlutterProjects\Flutter-Movie-List-App"
+export "FLUTTER_APPLICATION_PATH=C:\Users\Admin\FlutterProjects\Cinema"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build\ios"
